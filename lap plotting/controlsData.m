ba95a053af397e1f54b43d4ef848cfa4ab6b03e3
Michael Huang <michael_huang@berkeley.edu>
2022-04-17 13:52:12 -0700

    implement janky turning

---
+++ lap plotting/controlsData.m
@@ -0,0 +1,31 @@
+%% reset system, initialize track points, see file for layout
+clear;
+clc;
+trackDataSimple;
+nPoints = max(size(Length));
+output = [0,0];
+t = [5];
+ang = [];
+% velocity (m/s)
+v = 15;
+
+%% loop through each section of lap
+for n=1:1:nPoints
+   L = Length(n);
+   R = Radius(n);
+   DIR = Type(n);
+   
+   t = [t, t(end)+L/v];
+   t0 = round(t(end-1),2);
+   switch DIR
+       case "Straight"
+           ang = [ang,0];  
+       case "Left"
+           ang = [ang, L/R];
+       case "Right"
+           ang = [ang, -L/R];
+            
+   end
+   ang0 = round(ang(end),2);
+   output = [output; t0, ang0];
+end
---
+++ lap plotting/trackDataSimple.m
@@ -0,0 +1,3 @@
+Type = ["Straight", "Left", "Straight","Right", "Straight", "Right", "Straight", "Right", "Straight"];
+Length = [30, 10*pi(), 30, 10*pi(), 60, 10*pi(), 30, pi()*10, 30];
+Radius = [0, 10, 0,10,0, 10, 0, 10, 0];
